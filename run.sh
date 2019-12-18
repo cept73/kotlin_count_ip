fdb25b83a3afe941bceec06b4fdb31f998a03652
Sergey T <cept73@yandex.ru>
2019-12-19 00:57:48 +0300

    Added timer

--- run.sh
+++ run.sh
@@ -4,6 +4,8 @@
 APP_FILE=_build/main.jar
 
 # Проверяем существование файла, если есть - запускаем
-if [ -f "$APP_FILE" ]; then java -jar $APP_FILE $1
-else echo Сначала скомпилируйте программу с помощью build.sh
+if [ -f "$APP_FILE" ]; then
+    time -p java -jar $APP_FILE $1
+else
+    echo Сначала скомпилируйте программу с помощью build.sh
 fi
