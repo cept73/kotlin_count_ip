4ae2257fdf0663b454d89dce2627dfcba6933e55
Sergey Taranov <cept73@yandex.ru>
2019-12-22 19:42:47 +0400

    v1.2

--- main.kt
+++ main.kt
@@ -1,7 +1,7 @@
 /**
  * Определение количества IP адресов в файле
  * ===============================================================
- * v1.1 by Cept
+ * v1.2 by Cept
  *
  * В качестве параметра коммандной строки нужно передать имя файла
  * либо будет использоваться из DEFAULT_INPUT_FILENAME
@@ -9,196 +9,228 @@
 package ip_count
 
 import java.io.File // Требуется работа с файлами
-import java.math.BigInteger // Будем вместо хэшей засовывать в целое
+
+class App {
+
+    // Конфигурация
+    val DEFAULT_INPUT_FILENAME = "ips-list.txt"
+    var debugMode = true
 
 
-// Конфигурация
-val DEFAULT_INPUT_FILENAME: String = "ips-list.txt"
-var DEBUG: Boolean = true
-
-// Компоненты для хранения
-data class IPAddressComponents(
-    val addressToNode   : Long,
-    val addressToPoint  : Int
-)
-
-val INVALID = IPAddressComponents(0, 0)
+    // Информация об адресе
+    data class IPAddress(val addressToNode: Long, val addressToPoint: Int)
+    val INVALID_IPADDRESS = IPAddress(0, 0)
 
 
-// Результат выполнения
-var ipMap = mutableMapOf<Long, BigInteger>()
-var ipCount: Int = 0
-val powerOf2 = mutableMapOf<Int, BigInteger>()
+    // Результат выполнения
+    var ipMap = mutableMapOf<Long, LongArray>()
+    var ipCount: Int = 0
+    val powerOf2 = mutableMapOf<Int, Long>()
 
 
-/**
- * Главная программа
- * 
- * @params аргументы коммандной строки
- */
-fun main(args: Array<String>) {
-    // Определяемся с входным файлом
-    val inputFileName = getFileNameFrom(args)
+    /**
+     * Главная программа
+     *
+     * @params аргументы коммандной строки
+     */
+    fun run(args: Array<String>) {
+        // Определяемся с входным файлом
+        val inputFileName = getFileNameFrom(args)
 
-    // Считаем степени 2-ки
-    initPowerOf2()
+        // Считаем степени 2-ки
+        initPowerOf2()
 
-    // Ждем от пользователя действительное имя файла
-    if (!isFileExists( inputFileName )) {
-        println("Файл $inputFileName не существует. "
-            + "Введите имя исходного файла в коммандной строке.")
-        return
+        // Ждем от пользователя действительное имя файла
+        if (!isFileExists( inputFileName )) {
+            println("File $inputFileName is not exists. "
+                    + "Enter filename in command line.")
+            return
+        }
+
+        // Парсим файл
+        try {
+            parseFileLines(inputFileName, ::parserFun)
+        }
+        catch (e: Exception) {
+            println("Fatal error:")
+            println(e)
+        }
+        finally {
+            if (debugMode) {
+                ipMap.forEach {
+                    val (nodeAddr, segments) = it
+
+                    print("$nodeAddr -> ")
+                    for (item in segments) {
+                        print("${item.toString(16)} ")
+                    }
+                    println()
+                }
+            }
+            println("\nADDRESS COUNT: $ipCount\n")
+        }
     }
 
-    // Парсим файл
-    try {
-        parseFileLines(inputFileName, ::parserFun)
-    }
-    catch (e: Exception) {
-        println("Фатальная ошибка:")
-        println(e)
-    }
-    finally {
-        if (DEBUG) println(ipMap)
-        println("\nКоличество адресов: $ipCount\n")
-    }
-}
+    /**
+     * Основная функция
+     */
+    //@Suppress("UNUSED_VARIABLE")
+    // addressSpace не используем, иначе будет предупреждение
+    private fun parserFun(it: Any?): Unit {
+        // Разбираем IP из строки
+        val ipParsed = ipAddressParse(it as String)
 
+        // Неправильный адрес?
+        if (ipParsed == INVALID_IPADDRESS) return
 
-/**
- * Основная функция
- */
-//@Suppress("UNUSED_VARIABLE")
-// addressSpace не используем, иначе будет предупреждение
-private fun parserFun(it: Any?): Unit {
-    // Разбираем IP из строки
-    val ipParsed = ipAddressParse(it as String)
+        // Разложим адрес на ноду, поинта, тип адреса и порт
+        val (addressToNode, addressToPoint) = ipParsed
 
-    // Неправильный адрес?
-    if (ipParsed == INVALID) return
+        // Уже был такой адрес? Пропускаем
+        val stateOld = getSegmentState( addressToNode, addressToPoint )
+        val stateNew = getSegmentStateWithPoint( stateOld, addressToPoint )
 
-    // Разложим адрес на ноду, поинта, тип адреса и порт
-    val (addressToNode, addressToPoint) = ipParsed
+        // Если еще не добавляли, состояние поменяется
+        if (stateNew != stateOld) {
+            // Добавляем в карту хэшей и радуемся
+            setSegmentState( addressToNode, addressToPoint, stateNew )
+            ipCount = ipCount + 1
 
-    // Уже был такой адрес? Пропускаем
-    val stateOld = getNodeState( addressToNode )
-    val stateNew = getStateWithPoint( stateOld, addressToPoint )
+            // Если нужно отладку, выводим IP
+            if (debugMode) {
+                println(it)
 
-    // Если еще не добавляли, состояние поменяется
-    if (stateNew != stateOld) {
-        // Добавляем в карту хэшей и радуемся
-        setNodeState( addressToNode, stateNew )
-        ipCount = ipCount + 1
-
-        // Если нужно отладку, выводим IP
-        if (DEBUG) {
-            println(it)
-            // Отключим при количестве больше 10
-            if (ipCount > 10) {
-                DEBUG = false
-                println("Отладка отключена")
+                // Отключим при количестве больше 10
+                if (ipCount > 10) {
+                    debugMode = false
+                    println("**Debug is turned off**")
+                }
             }
         }
     }
-}
 
 
-/* Считаем степени 2-ки от 0 до 255 степени */
-fun initPowerOf2()
-{
-    var value: BigInteger = BigInteger.valueOf(1)
-    for (index in 0..255) {
-        powerOf2[ index ] = value
-        value += value
-    }
-}
-
-
-fun getNodeState(addressToNode: Long): BigInteger =
-    ipMap[addressToNode] ?: BigInteger.ZERO
-
-
-fun setNodeState( addressToNode: Long, stateNew: BigInteger )
-{
-    ipMap[addressToNode] = stateNew
-}
-
-
-fun getStateWithPoint(nodeState: BigInteger, addressToPoint: Int) =
-    nodeState.or(powerOf2[addressToPoint])
-
-
-fun setNodeWithPointState(addressToNode: Long, stateNew: BigInteger?)
-{
-    ipMap[addressToNode] = stateNew ?: BigInteger.ZERO
-}
-
-
-/**
- * Считываем имя входного файла из коммандной строки
- */
-private fun getFileNameFrom(args: Array<String>): String = 
-    // Берем первый параметр либо значение по-умолчанию
-    when (args.size) {
-        0       -> DEFAULT_INPUT_FILENAME   // вызов без параметров
-        else    -> args[0]                  // указанный первым
+    /* Считаем степени 2-ки от 0 до 255 степени */
+    fun initPowerOf2()
+    {
+        var value: Long = 1 // BigInteger.valueOf(1)
+        for (index in 0..64) {
+            powerOf2[ index ] = value
+            value += value
+        }
     }
 
 
-/**
- * Проверка на существование файла
- */
-private fun isFileExists(fileName: String) = File(fileName).exists()
+    fun getPointSegment(addressToPoint: Int): Int = addressToPoint / 64
 
 
-/**
- * Просмотр файла, пробегая выполняем parser
- */
-private fun parseFileLines(fileName: String, parser: (message: Any?) -> Unit) =
-    File(fileName).forEachLine { parser(it) }
+    fun getNode(addressToNode: Long): LongArray = ipMap[addressToNode] ?: longArrayOf(0, 0, 0, 0)
 
 
-/*
- * Готовим парсинг IPv4
- * 
- * Для каждого IP (например 1.2.3.4)
- * 1) берется первые три числа - называем это нодой. 
- *      (в данном случае 1.2.3)
- * 2) каждое число от 0x00 до 0xFF. переводим в шестнадцатиричный Long
- *      (число 0x010203) 
- * 3) в массиве, где индекс - это адрес ноды из п.2 храним
- *      упакованные поинты в бинарном виде.
- *      сначала там 256 нулей - 32 байта 
- *      (используем BigInteger, TODO: переделать в 4 Long)
- *      Когда встречаем нового поинта, заносим в соответствующий бит.
- *      Если уже установлен бит 1, то пропускаем
- *
- * Итого:
- *      На каждую ноду используем 3 байта на индекс и до 32 байт
- *      на хранение поинтов. Максимальное число нод - 256*256*256
- *      = 16 777 216 (16M), в каждой по 32Б => 
- *      Максимальный размер в памяти 512MB
- */
-fun ipAddressParse(ipAddress: String): IPAddressComponents {
-    var nodeAddress: Long = 0
+    fun getSegmentState(addressToNode: Long, addressToPoint: Int): Long
+    {
+        // Находим данные о ноде и адресе
+        val node = getNode(addressToNode)
+        val segment = getPointSegment(addressToPoint)       
 
-    // Разбиваем адрес на куски
-    val octets = ipAddress.split('.').reversed().toTypedArray()
-
-    // Вычисляем хэш
-    for (i in 1..3) {
-        val num = octets[i].toLong()
-        nodeAddress = nodeAddress.or(
-            num shl ((i - 1) * 8)
-            )
+        // Возвращаем сегмент из нее
+        return node[segment]
     }
 
-    val lastAddressPart = octets[0].toInt()
 
-    // Возвращаем
-    // -> (nodeIP, pointIndex, "IPv4", -1 если не указан)
-    return IPAddressComponents(
-        nodeAddress,
-        lastAddressPart
-        )
+    fun setSegmentState( addressToNode: Long, addressToPoint: Int, stateNew: Long)
+    {
+        // Находим данные о ноде и адресе
+        val segment = getPointSegment(addressToPoint)
+
+        // Is initialized?
+        val testerOfExistance = ipMap[addressToNode]?.get(0)
+        if (testerOfExistance == null)
+            ipMap[addressToNode] = longArrayOf(0, 0, 0, 0)
+
+        // Set value
+        ipMap[addressToNode]?.set(segment, stateNew)
+    }
+
+
+    fun getSegmentStateWithPoint(segmentState: Long, addressToPoint: Int): Long 
+    {
+        val addressBitIndex: Int = addressToPoint % 64
+        return segmentState.or( powerOf2[ addressBitIndex ] as Long )
+    }
+
+
+    /**
+     * Считываем имя входного файла из коммандной строки
+     */
+    private fun getFileNameFrom(args: Array<String>): String =
+            // Берем первый параметр либо значение по-умолчанию
+            when (args.size) {
+                0       -> DEFAULT_INPUT_FILENAME   // вызов без параметров
+                else    -> args[0]                  // указанный первым
+            }
+
+
+    /**
+     * Проверка на существование файла
+     */
+    private fun isFileExists(fileName: String) = File(fileName).exists()
+
+
+    /**
+     * Просмотр файла, пробегая выполняем parser
+     */
+    private fun parseFileLines(fileName: String, parser: (message: Any?) -> Unit) =
+            File(fileName).forEachLine { parser(it) }
+
+
+    /*
+     * Готовим парсинг IPv4
+     *
+     * Для каждого IP (например 1.2.3.4)
+     * 1) берется первые три числа - называем это нодой.
+     *      (в данном случае 1.2.3)
+     * 2) каждое число от 0x00 до 0xFF. переводим в шестнадцатиричный Long
+     *      (число 0x010203)
+     * 3) в массиве, где индекс - это адрес ноды из п.2 храним
+     *      упакованные поинты в бинарном виде.
+     *      сначала там 256 нулей - 32 байта
+     *      (используем BigInteger, TODO: переделать в 4 Long)
+     *      Когда встречаем нового поинта, заносим в соответствующий бит.
+     *      Если уже установлен бит 1, то пропускаем
+     *
+     * Итого:
+     *      На каждую ноду используем 3 байта на индекс и до 32 байт
+     *      на хранение поинтов. Максимальное число нод - 256*256*256
+     *      = 16 777 216 (16M), в каждой по 32Б =>
+     *      Максимальный размер в памяти 512MB
+     */
+    fun ipAddressParse(ipAddress: String): IPAddress {
+        var nodeAddress: Long = 0
+
+        // Разбиваем адрес на куски
+        val octets = ipAddress.split('.').reversed().toTypedArray()
+
+        // Вычисляем цифровой адрес ноды
+        for (i in 1..3) {
+            val num = octets[i].toLong()
+            val mask = num shl ((i - 1) * 8)
+            nodeAddress = nodeAddress.or(mask)
+        }
+
+        // Адрес поинта
+        val pointAddress = octets[0].toInt()
+
+        // Возвращаем
+        // -> (nodeIP, pointIndex, "IPv4", -1 если не указан)
+        return IPAddress(nodeAddress, pointAddress)
+    }
+
+}
+
+
+
+fun main(args: Array<String>)
+{
+    App().run(args)
 }
