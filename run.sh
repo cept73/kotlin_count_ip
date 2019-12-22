fd4776abd8996146e6b59e39c887f317d5da0681
Sergey Taranov <cept73@yandex.ru>
2019-12-22 19:43:05 +0400

    Run script updated

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ APP_FILE=_build/main.jar
 
 # Проверяем существование файла, если есть - запускаем
 if [ -f "$APP_FILE" ]; then
-    time -p java -jar $APP_FILE $1
+    time -p kotlin $APP_FILE $1
 else
     echo Сначала скомпилируйте программу с помощью build.sh
 fi
